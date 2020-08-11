6a65bb283f5deabd59b9f9df7489ac0b38784259
Uncurated Tests <git-10508765@vercel.com>
2020-08-11 16:27:20 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import {
+  Flex
+} from "@chakra-ui/core";
+
+export default () => {
+  return (
+    <Flex direction="column">
+    </Flex>
+  )
+}
---
+++ src/components/Iframe.js
@@ -0,0 +1,10 @@
+export default function Iframe({ src, style, title, ...rest }) {
+  return (
+    <iframe
+      src={src}
+      style={style}
+      title={title}
+      {...rest}
+    />
+  )
+}
